042a90c1d7067fc122a27f0cfaf2269d89724e88
PriceSt1 <elborja15@gmail.com>
2021-10-07 12:16:57 +0200

    Init 2

--- YoutubeDownloader/__init__.py
+++ YoutubeDownloader/__init__.py
@@ -4,7 +4,12 @@ from pytube import *
 yt = YouTube(input("Introduce el link de descarga: "))
 title = yt.title
 author = yt.author
-stream = Stream
+stream = StreamQuery.get_highest_resolution()
+
+
+
+stream.download(yt)
+
 
 filesize = stream.filesize
 
