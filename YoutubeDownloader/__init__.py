0476af9103dd358d7a635ae1f524a24daa5fb060
PriceSt1 <33360950+PriceSt1@users.noreply.github.com>
2021-10-07 21:36:12 +0200

    Programa al 80%

--- YoutubeDownloader/__init__.py
+++ YoutubeDownloader/__init__.py
@@ -1,37 +1,88 @@
 from pytube import *
 import math
-from YoutubeDownloader import Path
-
-
-def direccionDescarga():
-        return Path.get_download_folder()
+import os
+import ctypes
+from ctypes import windll, wintypes
+from uuid import UUID
 
 def main():
-    yt = YouTube(input("Introduce el link"))
-    print("Cargando...")
-    title = yt.title
-    author = yt.author
-    stream = yt.streams.get_highest_resolution()
-    downloadPath = direccionDescarga()
-    print("Cargando...")
-    filesize = stream.filesize
-    filesize = (filesize) / math.pow(2, 20)
-    filesize = round(filesize, 2)
-    print("Cargando...")
+    salir=True
+    while(salir):
+        yt = YouTube(input("Introduce el link: "))
+        print("Cargando...")
+        title = yt.title
+        author = yt.author
+        stream = yt.streams.get_highest_resolution()
+        downloadPath = get_download_folder()
+        print("Cargando...")
+        filesize = stream.filesize
+        filesize = (filesize) / math.pow(2, 20)
+        filesize = round(filesize, 2)
+        print("Cargando...")
 
 
 
 
-    print(f"Descargando video \n "
+        print(f"Descargando video \n "
             f"Titulo: {title} \n "
             f"Autor: {author} \n "
             f"carpeta: {downloadPath} \n "
             f"El video ocupa {filesize} MiB")
 
-    print("Iniciando descarga")
+        print("Iniciando descarga")
 
-    stream.download(output_path=downloadPath)
+        stream.download(output_path=downloadPath)
 
-    print("Descarga terminada")
+        print("Descarga terminada")
+
+        salida=input("Quieres salir? [Y/N]")
+        if salida=="Y":
+            salir=False
+
+
+
+
+
+if os.name == 'nt':
+
+    # ctypes GUID copied from MSDN sample code
+    class GUID(ctypes.Structure):
+        _fields_ = [
+            ("Data1", wintypes.DWORD),
+            ("Data2", wintypes.WORD),
+            ("Data3", wintypes.WORD),
+            ("Data4", wintypes.BYTE * 8)
+        ]
+
+        def __init__(self, uuidstr):
+            uuid = UUID(uuidstr)
+            ctypes.Structure.__init__(self)
+            self.Data1, self.Data2, self.Data3, \
+                self.Data4[0], self.Data4[1], rest = uuid.fields
+            for i in range(2, 8):
+                self.Data4[i] = rest>>(8-i-1)*8 & 0xff
+
+    SHGetKnownFolderPath = windll.shell32.SHGetKnownFolderPath
+    SHGetKnownFolderPath.argtypes = [
+        ctypes.POINTER(GUID), wintypes.DWORD,
+        wintypes.HANDLE, ctypes.POINTER(ctypes.c_wchar_p)
+    ]
+
+    def _get_known_folder_path(uuidstr):
+        pathptr = ctypes.c_wchar_p()
+        guid = GUID(uuidstr)
+        if SHGetKnownFolderPath(ctypes.byref(guid), 0, 0, ctypes.byref(pathptr)):
+            raise ctypes.WinError()
+        return pathptr.value
+
+    FOLDERID_Download = '{374DE290-123F-4565-9164-39C4925E467B}'
+
+    def get_download_folder():
+        return _get_known_folder_path(FOLDERID_Download)
+else:
+    def get_download_folder():
+        home = os.path.expanduser("~")
+        return os.path.join(home, "Downloads")
+
 if __name__ == '__main__':
         main()
